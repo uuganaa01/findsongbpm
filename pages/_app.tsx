26ccbc73a7bdfc4980f18e86ad4ae83bd953344f
uuganaa01 <devuloper@gmail.com>
2021-01-07 18:06:35 +0800

    google analytics added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,23 @@
 import type { AppProps } from "next/app";
 import "../styles/theme.css";
 // import "../styles/custom.css";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/dist/client/router";
+import { useEffect } from "react";
 
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
-function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
-}
+};
 
-export default MyApp;
+export default App;
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,6 @@
 import NextDocument, { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
+const { GA_TRACKING_ID } = process.env;
 
 type Props = {};
 
@@ -11,6 +12,19 @@ class Document extends NextDocument<Props> {
         {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/uikit@3.6.7/dist/css/uikit.min.css" /> */}
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.14/dist/js/uikit.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/uikit@3.5.16/dist/js/uikit-icons.min.js"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-2CZYMGR7TE"></script>
+        <script
+            dangerouslySetInnerHTML={{
+              __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', '${GA_TRACKING_ID}', {
+                page_path: window.location.pathname,
+              });
+          `
+            }}
+          />
         <body>
           <Main />
           <NextScript />
